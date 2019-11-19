9315c9ef8fc91228b0c53f4131467e362c7a4791
PrieurA <alexandre-prieur_student2019@wilder.school>
2019-11-19 09:48:42 +0100

    comparable & comparator exercise

--- Thanos.java
+++ Thanos.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Thanos {
 
@@ -13,12 +15,22 @@ public class Thanos {
 
         // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
 
+        Collections.sort(heroes);
 
         System.out.println("\nOrder by name:");
         showList(heroes);
 
         // TODO 2: Add a Comparator and sort by age (descending)
 
+        Comparator<Hero> comparator = new Comparator<Hero>() {
+
+            @Override
+            public int compare(Hero hero1, Hero hero2) {
+                return hero2.getAge() - hero1.getAge();
+            }
+        };
+
+        Collections.sort(heroes, comparator);
 
         System.out.println("\nOrder by age:");
         showList(heroes);
